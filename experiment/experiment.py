0b3212950e2f2a882aa70893aef7ac9fe2b12d0d
Always-tt <tt20200220i@163.com>
2021-04-01 22:10:00 +0800

    琐碎

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -24,10 +24,10 @@
 #     else:
 #         break
 
-from entity.manager import ManagerPeoples
-from entity.manager import ManagerLessons
-from entity.people import People
-from view.land import Land
+# from entity.manager import ManagerPeoples
+# from entity.manager import ManagerLessons
+# from entity.people import People
+# from view.land import Land
 
 # manager = ManagerPeoples()
 # id_number = 123
@@ -51,3 +51,27 @@ from view.land import Land
 # test_2 = ManagerLessons(person_2)
 # test_2.cancel_lesson("电路", 1)
 # test_2.save_lessons()
+from usecase.manager import ManagerPeoples
+a = ManagerPeoples()
+person = a.find_person_by_id(1)
+person.password = '1'
+a.save_peoples()
+print(person.password)
+
+# model = self.teacher_lessons_tree_view.get_model()
+# _iter = model[0]
+# print(_iter[0])
+# model.remove(_iter.iter)
+#     _iter = self.model[0]
+#     name = _iter[0]
+#     print(name)
+#     self.model.remove(_iter.iter)
+# ui = AddLessonUI(self._person)
+# ui.window.show()
+# self.model.
+# def update(self):
+#     while Gtk.events_pending():
+#         Gtk.main_iteration()
+#         self.load_tree_view()
+# def is_show(self):
+
